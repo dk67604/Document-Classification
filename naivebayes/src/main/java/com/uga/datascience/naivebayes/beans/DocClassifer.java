de29c7b7980b7d8fea42a6ddf3f8d87f3c7604fc
dk67604 <dk67604@uga.edu>
2016-09-03 21:12:08 -0400

    Update DocClassifer.java
    Added Comment

--- naivebayes/src/main/java/com/uga/datascience/naivebayes/beans/DocClassifer.java
+++ naivebayes/src/main/java/com/uga/datascience/naivebayes/beans/DocClassifer.java
@@ -2,27 +2,60 @@ package com.uga.datascience.naivebayes.beans;
 
 import java.io.Serializable;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DocClassifer.
+ */
 public class DocClassifer implements Serializable {
 
-	/**
-	 * 
-	 */
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -2699734172611548052L;
+	
+	/** The doc id. */
 	private String docId;
+    
+    /** The label. */
     private String label;
     
+	/**
+	 * Gets the doc id.
+	 *
+	 * @return the doc id
+	 */
 	public String getDocId() {
 		return docId;
 	}
+	
+	/**
+	 * Sets the doc id.
+	 *
+	 * @param docId the new doc id
+	 */
 	public void setDocId(String docId) {
 		this.docId = docId;
 	}
+	
+	/**
+	 * Gets the label.
+	 *
+	 * @return the label
+	 */
 	public String getLabel() {
 		return label;
 	}
+	
+	/**
+	 * Sets the label.
+	 *
+	 * @param label the new label
+	 */
 	public void setLabel(String label) {
 		this.label = label;
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "DocClassifer [docId=" + docId + ", label=" + label + "]";
